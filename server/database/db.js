c08dbdbfb2b818d49ff5390c946ccd5cf9f5e47f
sahil13082003 <sahilgolhar7@gmail.com>
2024-11-01 09:29:17 +0530

    atlas uri added and nodemon changes to node

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const Connection = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/Crud-app');
+    // await mongoose.connect('mongodb://localhost:27017/Crud-app');
+    await mongoose.connect('mongodb+srv://sahilgolhar7:rdsIfa7pkrDXt0EW@cluster0.fbsb4.mongodb.net/MERN_CRUD_Operation?retryWrites=true&w=majority&appName=Cluster0');
+
+
     console.log('Connected to the database');
   } catch (error) {
     console.error('Error connecting to the database:', error.message);
